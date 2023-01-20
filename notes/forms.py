10e05d2fadf12705c9ed360f3a35e5b0db3c888f
いず <ritu6811@gmail.com>
2023-01-20 22:14:08 +0900

    add datepicker & jquery

--- notes/forms.py
+++ notes/forms.py
@@ -1,18 +1,25 @@
+"""
+HTMLフォームの生成、値のValidationに利用できる。
+"""
+
+from bootstrap_datepicker_plus.widgets import DatePickerInput
 from django import forms
 
 from notes.models import Kakeibo
 
-"""
-HTMLフォームの生成、値のValidationに利用できる。
-"""
+
 class NotesForm(forms.ModelForm):
+
     class Meta:
         model = Kakeibo
         fields = (
-            'title', 
-            'money', 
-            'tag', 
-            'trading_dt', 
+            'title',
+            'money',
+            'tag',
+            'trading_dt',
             'io_flag'
             )
+        widgets = {
+            'trading_dt': DatePickerInput(),
+            }
         
